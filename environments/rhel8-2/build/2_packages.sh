6f688aa9848f65dd41ea5470d2107ffc75af758f
eneuhauss <eneuhauss@dg-i.net>
2020-10-03 23:30:08 +0200

    produce loop devices and install less software for faster setup

--- environments/rhel8-2/build/2_packages.sh
+++ environments/rhel8-2/build/2_packages.sh
@@ -1,15 +1,16 @@
 echo "Installing RHEL8 Packages"
-sudo yum install -y buildah podman wget vim emacs git sudo tmux cockpit ca-certificates httpd mod_ssl
+#sudo yum install -y buildah podman wget vim emacs git sudo tmux cockpit ca-certificates httpd mod_ssl
+sudo yum install -y wget vim emacs git sudo tmux ca-certificates bash-completion
 
-# To speed up installation of bcc-tools lets pre-install some dependencies
-sudo yum install -y gcc llvm-libs gcc-c++ cpp binutils bash-completion
-
-sudo systemctl enable --now cockpit.socket
-
-adduser rhel
-sudo usermod -aG wheel rhel
-echo redhat | sudo passwd --stdin rhel
-
-touch /home/rhel/.hushlogin
-mkdir /home/rhel/.ssh
-echo 'rhel    ALL=(ALL)       NOPASSWD: ALL' >>  /etc/sudoers
+## To speed up installation of bcc-tools lets pre-install some dependencies
+#sudo yum install -y gcc llvm-libs gcc-c++ cpp binutils bash-completion
+#
+#sudo systemctl enable --now cockpit.socket
+#
+#adduser rhel
+#sudo usermod -aG wheel rhel
+#echo redhat | sudo passwd --stdin rhel
+#
+#touch /home/rhel/.hushlogin
+#mkdir /home/rhel/.ssh
+#echo 'rhel    ALL=(ALL)       NOPASSWD: ALL' >>  /etc/sudoers
---
+++ environments/rhel8-2/build/4_loop_device.sh
@@ -0,0 +1,7 @@
+set -ex
+
+truncate -s 10g /tmp/loop0.img
+losetup /dev/loop0 /tmp/loop0.img
+
+truncate -s 10g /tmp/loop1.img
+losetup /dev/loop1 /tmp/loop0.img
