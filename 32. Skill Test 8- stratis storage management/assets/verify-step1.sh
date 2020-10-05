0bb38ec42eba8b679ab39e14a9abbe7ce660b53a
eneuhauss <eneuhauss@dg-i.net>
2020-10-05 20:07:31 +0200

    about to create step2 here

--- 32. Skill Test 8- stratis storage management/assets/verify-step1.sh
+++ 32. Skill Test 8- stratis storage management/assets/verify-step1.sh
@@ -9,10 +9,10 @@ RESET=$(tput sgr0)
 ################################
 #Description of function: Print a given message in color
 #Arguments:"color""text"
-#Colors accepted: green red 
+#Colors accepted: green red
 
 ################################
-function print_color(){ 
+function print_color(){
   case $1 in
     "green")COLOR="\033[0;32m";;
     "red")COLOR="\033[0;31m";;
@@ -43,5 +43,21 @@ ENABLED="$( systemctl status stratisd | grep -o 'service; enabled' )"
 comparison "${ENABLED}" 'service; enabled' "2"
 ACTIVE_RUNNING="$( systemctl status stratisd | grep -o 'Active: active (running)' )"
 comparison "${ACTIVE_RUNNING}" "Active: active (running)" "3"
+POOL_EXISTS="$( stratis pool | grep -o 'pool0' )"
+comparison "${POOL_EXISTS}" "pool0" "4"
+DEV_EXISTS="$( stratis blockdev list | grep -o -P 'loop[01]' | wc -l )"
+comparison "${DEV_EXISTS}" "2" "5"
+FS1_EXISTS="$( stratis filesystem list | grep -o '/stratis/pool0/stratis_fs1' )"
+comparison "${FS1_EXISTS}" "/stratis/pool0/stratis_fs1" "6"
+FS2_EXISTS="$( stratis filesystem list | grep -o '/stratis/pool0/stratis_fs1' )"
+comparison "${FS2_EXISTS}" "/stratis/pool0/stratis_fs2" "7"
+MOUNT=$( mount | grep stratis_f | wc -l)
+comparison "${MOUNT}" "2" "8"
+FSTAB="$( grep -qP '^UUID.+xfs.+defaults,x-systemd.requires=stratisd.service.+0.+0$' /etc/fstab && echo $? )"
+comparison "${FSTAB}" "0" "9"
+FILE=$( [ -f /mnt/fs1/very_important_file ] && echo $? )
+comparison "${FILE}" "0" "10"
+SNAP=$( stratis filesystem | grep -q stratis_fs1-snapshot && echo $? )
+comparison "${SNAP}" "0" "10"
 
 print_color "green" "You Freaking Rock "
---
+++ 32. Skill Test 8- stratis storage management/step2.md
@@ -0,0 +1,11 @@
+## stratis storage management
+1. we have deleted `/mnt/fs1/very_important_file` you have created before
+2. recover the file from the snapshot and make sure it is there
+3. delete the snapshot
+4. 
+5. 
+6. 
+7. 
+
+
+Once complete please run the following command: `bash /tmp/verify.sh`{{execute}}
